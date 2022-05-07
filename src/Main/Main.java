5d9a3a2335fc9cfa710781f3b82ecb5af48ab6cb
Gyenes Dávid <gyenesdavid@gmail.com>
2022-05-07 10:52:18 +0200

    Lemaradt komment

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,10 +4,13 @@ package Main;
 import GUI.GUI;
 
 public class Main {
+
+    /**
+     * A fő függvény, ami mindent elindít.
+     * 
+     * @param args Bemeneti argumentumok, nem használjuk.
+     */
     public static void main(String[] args) {
-        /** A fő függvény, ami mindent elindít.
-         * @param args Bemeneti argumentumok, nem használjuk.
-         */
         GUI gui = new GUI();
         gui.start();
     }
